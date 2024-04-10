f0cbc8aa0e400232066a68cadb01c6cf21d3984e
igorkendyS <igor-kendy@hotmail.com>
2024-04-10 07:53:00 -0300

    args e kwargs, em andamento

--- funcoes/nomearArgumento.py
+++ funcoes/nomearArgumento.py
@@ -3,3 +3,5 @@ def salvar_carro(marca, modelo, ano, placa):
     
 salvar_carro("FIAT", "Palio", 1999, "ABC-1234")
 salvar_carro(ano=1999, placa="ABC-1234", marca="FIAT", modelo="Palio" )
+# dicionario
+salvar_carro(**{"marca": "fiat", "modelo": "Paloo", "ano": 1999, "placa": "ABC-1234"})
