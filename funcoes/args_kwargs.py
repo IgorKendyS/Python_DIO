234a962fe2e4371b744942860a2050e625080339
igorkendyS <igor-kendy@hotmail.com>
2024-04-11 19:43:18 -0300

    comit esquecido

--- funcoes/args_kwargs.py
+++ funcoes/args_kwargs.py
@@ -4,4 +4,4 @@ def exibir_poema(data_extenso, *args, **kwargs):
     mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
     
-exibir_poema("Zen of Python", "Beautiful is better than ugly.", author ="Time Peters", ano = 1999 )
+exibir_poema("Zen of Python", "Beautiful is better than ugly.", "Beautiful is better than ugly.", "Beautiful is better than ugly.", author ="Time Peters", ano = 1999 )
