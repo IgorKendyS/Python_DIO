87771c3f1a867a6259fb399bd5fa19391e9173aa
igorkendyS <igor-kendy@hotmail.com>
2024-04-19 17:13:32 -0300

    aulas de heranca terminada

--- Heranca/heranca_multipla.py
+++ Heranca/heranca_multipla.py
@@ -6,14 +6,20 @@ class Animal:
         return f"{self.__class__.__name__}: {', '.join([f'{chave}:{valor}' for chave, valor in self.__dict__.items()])}"
 
 class Mamifero(Animal):
-    def __init__(self, nro_patas, cor_pelo):
-        super().__init__(nro_patas)
-        self.nro_patas = nro_patas
+    def __init__(self, cor_pelo, **kw):
+        super().__init__(**kw)
         self.cor_pelo = cor_pelo
+        
+    def __str__(self):
+        return __class__.__name__
 
 class Ave(Animal):
-    def __init__(self, nro_patas, cor_bico):
-        super().__init__(nro_patas)
+    def __init__(self, cor_bico, **kw):
+        super().__init__(**kw)
+        self.cor_bico = cor_bico
+        
+    def __str__(self):
+        return "ave 42"
 
 class Cachorro(Mamifero):
     pass
@@ -25,11 +31,19 @@ class Leao(Mamifero):
     pass
 
 class Ornitorrinco(Mamifero, Ave):
-    pass
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        
+        #print(Ornitorrinco.__mro__)
+        print(Ornitorrinco.mro())
+        
+        super().__init__(cor_bico=cor_bico, cor_pelo=cor_pelo, nro_patas=nro_patas)
+        
+    def __str__(self):
+        return self.__class__.__name__
 
 
-gato = Gato(4, 'preto')
+gato = Gato(nro_patas=4, cor_pelo='preto')
 print(gato)
 
-ornitorrinco = Ornitorrinco(4, "marrom", "laranja")
+ornitorrinco = Ornitorrinco(nro_patas=4, cor_pelo="marrom", cor_bico="laranja")
 print(ornitorrinco)
